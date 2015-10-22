932a78f9a0a94ef24bf2ac164d94f2bffaf5278c
wayne <waynehui77@hotmail.com>
2015-10-23 06:06:33 +1100

    Initial Commit

---
+++ Infuse Beacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Infuse Beacon
+//
+//  Created by waynehui on 23/10/15.
+//  Copyright © 2015年 waynehui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
